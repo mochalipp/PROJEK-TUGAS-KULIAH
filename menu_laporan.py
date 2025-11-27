4d5eaa4c617c60c69972451c64b933333f87a950
mochalipp <mchdalief24@gmail.com>
2025-11-27 14:11:26 +0700

    nampilin nama siswa

--- menu_laporan.py
+++ menu_laporan.py
@@ -3,6 +3,27 @@ import os
 
 FILE_NAME = 'data_beasiswa.xlsx'
 
+# Fungsi mendapatkan nama siswa berdasarkan NISN 
+def get_nama_siswa(workbook, nisn):
+    if 'Siswa' not in workbook.sheetnames:
+        return "-"
+    sheet = workbook['Siswa']
+    for row in sheet.iter_rows(min_row=2, values_only=True):
+        if row[0] == nisn:
+            return row[1]
+    return "-"
+
+# fungsi mendapatkan nama beasiswa berdasarkan kode
+def get_nama_beasiswa(workbook, kode):
+    if 'Beasiswa' not in workbook.sheetnames:
+        return "-"
+    sheet = workbook['Beasiswa']
+    for row in sheet.iter_rows(min_row=2, values_only=True):
+        if row[0] == kode:
+            return row[1]
+    return "-"
+
+# LAPORAN DISTRIBUSI BEASISWA
 def tampil_laporan_distribusi():
     if not os.path.exists(FILE_NAME):
         print("File data beasiswa tidak ditemukan.")
@@ -20,11 +41,17 @@ def tampil_laporan_distribusi():
         print("Belum ada data distribusi beasiswa.")
         return
 
-    print("\n=== Laporan Distribusi Beasiswa ===")
-    for row in sheet.iter_rows(min_row=1, values_only=True):
-        print("{:<15} {:<20} {:<15}".format(*row))
-        print("-" * 50)
+    print("\n=== LAPORAN DISTRIBUSI BEASISWA ===")
+    print("{:<15} {:<20} {:<20} {:<15}".format("NISN", "Nama Siswa", "Nama Beasiswa", "Tanggal"))
+    print("-" * 70)
 
+    for nisn, kode, tanggal in sheet.iter_rows(min_row=2, values_only=True):
+        nama_siswa = get_nama_siswa(workbook, nisn)
+        nama_bea = get_nama_beasiswa(workbook, kode)
+
+        print("{:<15} {:<20} {:<20} {:<15}".format(nisn, nama_siswa, nama_bea, tanggal))
+
+# MENU LAPORAN
 def menu_laporan():
     while True:
         print("\n === MENU LAPORAN ===")
